03b3a3f78063e2779689108c9ffd888d39f27b1b
Luffy <ponosonik1@mail.ru>
2023-10-14 12:43:20 +0500

    code review 2

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 //console.log("alax");
 //import Swiper from "swiper";
+
 import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
@@ -7,4 +8,29 @@ flsFunctions.isWebp();
 import Swiper, { Navigation, Pagination } from 'swiper';
 
 const swiper = new Swiper();
+
+
+
+const buttons = document.querySelectorAll('.my-button');
+
+// Добавляем обработчик события для каждой кнопки
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        for(let i = 0; i < 3; i++){
+            //можно найти первый объект определёного цвета и поменять цвет на другой
+            var elementsWithBorderClass = document.querySelectorAll(".my-button");
+        
+            // Применяем красный цвет к каждому найденному элементу
+            elementsWithBorderClass.forEach(function(element) {
+            element.style.background = "#D4E8D9";
+            element.style.color = "#2C2D35";
+            });
+        }
+        
+        
+        // Изменяем цвет кнопки, на которую нажали
+        button.style.backgroundColor = "#376B44";
+        button.style.color = "#F4F6F5";
+    });
+});
 
